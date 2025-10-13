ad96ee8b26fe1844ab3346ce07764a9840e2aa0d
Theo Faria <theocarfar@gmail.com>
2025-10-13 13:43:05 -0300

    feat: implementa a classe Personagem

---
+++ src/main/java/entidades/Personagem.java
@@ -0,0 +1,13 @@
+package entidades;
+
+import jdk.jshell.PersistentSnippet;
+
+public class Personagem {
+    private float vidaMaxima;
+    private float ataqueBase;
+    private float defesaBase;
+    private int alcance;
+
+    public void anda() {}
+    public void ataca(Personagem inimigo) {}
+}
